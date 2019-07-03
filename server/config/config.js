12bbdfca441c55496303c5b49a7769544f4850d2
Rf410 <ejale@outlook.com>
2019-07-02 23:54:41 -0400

    Comentario sobre variable de entorno personalizada

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
+    // MONGO_DB es una variable de enterno personalizada en heroku
     urlDB = process.env.MONGO_DB;
 }
 
